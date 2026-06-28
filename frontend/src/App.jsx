3fffbace42ba2d059e8ec820d70ef37f79614bc0
ritikmalakar <ritikmalakar445@gmail.com>
2026-06-28 09:12:54 +0530

    change frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import UserRoutes from "./routes/userRoutes";
+import UserRoutes from "./routes/UserRoutes";
 import Navbar from "./component/Navbar";
 import Footer from "./component/Footer";
 
