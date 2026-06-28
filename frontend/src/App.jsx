ef35c3b878339b297689d3e380fe8d186523d463
ritikmalakar <ritikmalakar445@gmail.com>
2026-06-28 10:31:12 +0530

    change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,6 @@ function App() {
 
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/*" element={<ErrorPage />} />
       </Routes>
       <Footer />
     </>
